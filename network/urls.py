41cb85fc6fc9e04b425823118e03aff962b62de9
niyaz <khasanchik85@gmail.com>
2026-01-23 21:56:08 +0500

    refactor: удалены CRUD операции из публичного интерфейса, оставлен только просмотр

--- network/urls.py
+++ network/urls.py
@@ -4,13 +4,7 @@ from . import views
 app_name = 'network'
 
 urlpatterns = [
-    # Главная страница сети
-    path('', views.EquipmentListView.as_view(), name='index'),
-    
-    # CRUD для оборудования
-    path('equipment/', views.EquipmentListView.as_view(), name='equipment_list'),
-    path('equipment/create/', views.EquipmentCreateView.as_view(), name='equipment_create'),
+    path('', views.EquipmentListView.as_view(), name='equipment_list'),
     path('equipment/<int:pk>/', views.EquipmentDetailView.as_view(), name='equipment_detail'),
-    path('equipment/<int:pk>/edit/', views.EquipmentUpdateView.as_view(), name='equipment_update'),
-    path('equipment/<int:pk>/delete/', views.EquipmentDeleteView.as_view(), name='equipment_delete'),
+    
 ]
--- network/views.py
+++ network/views.py
@@ -1,10 +1,7 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib import messages
+from django.shortcuts import render
 from django.db.models import Q
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView
 from .models import Location, NetworkEquipment
-from .forms import NetworkEquipmentForm
 
 class EquipmentListView(ListView):
     model = NetworkEquipment
@@ -13,12 +10,6 @@ class EquipmentListView(ListView):
     
     def get_queryset(self):
         queryset = NetworkEquipment.objects.all()
-        
-        # Используем only() для загрузки только существующих полей
-        queryset = queryset.only(
-            'id', 'name', 'type', 'model', 'ip_address', 
-            'status', 'location_id'
-        )
         
         # Фильтрация по статусу
         status_filter = self.request.GET.get('status', '')
@@ -30,8 +21,10 @@ class EquipmentListView(ListView):
         if search_query:
             queryset = queryset.filter(
                 Q(name__icontains=search_query) |
-                Q(model__icontains=search_query)
-                # Убираем serial_number и inventory_number, так как их пока нет в базе
+                Q(model__icontains=search_query) |
+                Q(serial_number__icontains=search_query) |
+                Q(inventory_number__icontains=search_query) |
+                Q(ip_address__icontains=search_query)
             )
         
         return queryset
@@ -39,7 +32,7 @@ class EquipmentListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         
-        # Статистика (только с существующими полями)
+        # Статистика
         context['status_stats'] = {
             'active': NetworkEquipment.objects.filter(status='active').count(),
             'backup': NetworkEquipment.objects.filter(status='backup').count(),
@@ -58,62 +51,3 @@ class EquipmentDetailView(DetailView):
     model = NetworkEquipment
     template_name = 'network/equipment_detail.html'
     context_object_name = 'equipment'
-    
-    def get_queryset(self):
-        # Только существующие поля
-        return NetworkEquipment.objects.only(
-            'id', 'name', 'type', 'model', 'ip_address', 
-            'status', 'description', 'location_id'
-        )
-
-
-class EquipmentCreateView(CreateView):
-    model = NetworkEquipment
-    form_class = NetworkEquipmentForm
-    template_name = 'network/equipment_form.html'
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Добавить сетевое оборудование'
-        return context
-    
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        messages.success(self.request, f'Оборудование "{self.object.name}" успешно добавлено!')
-        return response
-    
-    def get_success_url(self):
-        return reverse_lazy('network:equipment_detail', kwargs={'pk': self.object.pk})
-
-
-class EquipmentUpdateView(UpdateView):
-    model = NetworkEquipment
-    form_class = NetworkEquipmentForm
-    template_name = 'network/equipment_form.html'
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = f'Редактировать: {self.object.name}'
-        context['equipment'] = self.object
-        return context
-    
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        messages.success(self.request, f'Оборудование "{self.object.name}" успешно обновлено!')
-        return response
-    
-    def get_success_url(self):
-        return reverse_lazy('network:equipment_detail', kwargs={'pk': self.object.pk})
-
-
-class EquipmentDeleteView(DeleteView):
-    model = NetworkEquipment
-    template_name = 'network/equipment_confirm_delete.html'
-    context_object_name = 'equipment'
-    success_url = reverse_lazy('network:equipment_list')
-    
-    def form_valid(self, form):
-        equipment_name = self.object.name
-        response = super().form_valid(form)
-        messages.success(self.request, f'Оборудование "{equipment_name}" удалено!')
-        return response
