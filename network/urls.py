bb2cc7be64a2a959eb1c167cf1a277f26ad46083
niyaz <khasanchik85@gmail.com>
2026-01-25 21:51:50 +0500

    Реализация IP-менеджера: поиск IP, список подсетей, навигация

--- network/urls.py
+++ network/urls.py
@@ -6,5 +6,9 @@ app_name = 'network'
 urlpatterns = [
     path('', views.EquipmentListView.as_view(), name='equipment_list'),
     path('equipment/<int:pk>/', views.EquipmentDetailView.as_view(), name='equipment_detail'),
-    
+    path('ip-lookup/', views.IPLookupView.as_view(), name='ip_lookup'),
+    path('subnets/', views.SubnetListView.as_view(), name='subnet_list'),
+    path('subnets/<int:pk>/', views.SubnetDetailView.as_view(), name='subnet_detail'),
+    path('network-map/', views.NetworkMapView.as_view(), name='network_map'),
+    path('dashboard/', views.NetworkMapView.as_view(), name='dashboard'),
 ]
--- network/views.py
+++ network/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Q
-from django.views.generic import ListView, DetailView
-from .models import Location, NetworkEquipment
+from django.views.generic import ListView, DetailView, TemplateView
+from .models import Location, NetworkEquipment, Subnet, IPAddress
 
 class EquipmentListView(ListView):
     model = NetworkEquipment
@@ -51,3 +51,116 @@ class EquipmentDetailView(DetailView):
     model = NetworkEquipment
     template_name = 'network/equipment_detail.html'
     context_object_name = 'equipment'
+
+class IPLookupView(TemplateView):
+    """Поиск: Кто на этом IP?"""
+    template_name = 'network/ip_lookup.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        ip = self.request.GET.get('ip', '')
+        result = None
+        
+        if ip:
+            result = IPAddress.objects.filter(address=ip).select_related('device', 'subnet').first()
+        
+        context['ip'] = ip
+        context['result'] = result
+        return context
+
+class SubnetListView(ListView):
+    """Список всех подсетей"""
+    model = Subnet
+    template_name = 'network/subnet_list.html'
+    context_object_name = 'subnets'
+    
+    def get_queryset(self):
+        return Subnet.objects.all().select_related('location')
+
+class SubnetDetailView(DetailView):
+    """Детали подсети + IP адреса"""
+    model = Subnet
+    template_name = 'network/subnet_detail.html'
+    context_object_name = 'subnet'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ip_addresses'] = IPAddress.objects.filter(
+            subnet=self.object
+        ).select_related('device')
+        return context
+
+class NetworkMapView(TemplateView):
+    """Топология сети - визуальная схема"""
+    template_name = 'network/network_map.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        # Получаем параметры фильтрации из GET-запроса
+        location_filter = self.request.GET.get('location')
+        type_filter = self.request.GET.get('type')
+        status_filter = self.request.GET.get('status')
+        
+        # Базовый запрос оборудования
+        devices = NetworkEquipment.objects.all().select_related('location')
+        
+        # Применяем фильтры
+        if location_filter:
+            devices = devices.filter(location_id=location_filter)
+        
+        if type_filter:
+            devices = devices.filter(type=type_filter)
+        
+        if status_filter:
+            devices = devices.filter(status=status_filter)
+        
+        # Группируем оборудование по локациям для топологии
+        locations_data = {}
+        
+        for device in devices:
+            location_id = device.location_id if device.location else 0
+            location_name = device.location.name if device.location else "Без локации"
+            
+            if location_id not in locations_data:
+                locations_data[location_id] = {
+                    'id': location_id,
+                    'name': location_name,
+                    'devices': [],
+                    'device_stats': {
+                        'router': 0, 'switch': 0, 'server': 0, 
+                        'firewall': 0, 'access_point': 0, 'other': 0
+                    }
+                }
+            
+            locations_data[location_id]['devices'].append(device)
+            
+            # Считаем типы оборудования
+            device_type = device.type if device.type else 'other'
+            if device_type in locations_data[location_id]['device_stats']:
+                locations_data[location_id]['device_stats'][device_type] += 1
+            else:
+                locations_data[location_id]['device_stats']['other'] += 1
+        
+        # Преобразуем в список
+        network_topology = list(locations_data.values())
+        
+        # Считаем общее количество устройств в топологии
+        filtered_devices_count = sum(len(location['devices']) for location in network_topology)
+        
+        # Получаем все локации для фильтра
+        all_locations = Location.objects.all()
+        
+        # Статистика
+        context.update({
+            'network_topology': network_topology,
+            'all_locations': all_locations,
+            'total_devices': devices.count(),
+            'filtered_devices_count': filtered_devices_count,  # Добавляем
+            'total_locations': len(network_topology),
+            'location_filter': location_filter,
+            'type_filter': type_filter,
+            'status_filter': status_filter,
+        })
+        
+        return context
